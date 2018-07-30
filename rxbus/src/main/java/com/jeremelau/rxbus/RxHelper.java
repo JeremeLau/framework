a42237aa6d808a26c03c7ea25c36d7cf145b91ea
Jereme <jeremegeit@pm.me>
2018-07-30 15:45:15 +0800

    add RxHelper

---
+++ rxbus/src/main/java/com/jeremelau/rxbus/RxHelper.java
@@ -0,0 +1,18 @@
+package com.jeremelau.rxbus;
+
+import io.reactivex.ObservableTransformer;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.schedulers.Schedulers;
+
+/**
+ * Author: Created by jereme on 2018/7/30
+ * E-main: liuqx@guoguang.com.cn
+ */
+public class RxHelper {
+    public RxHelper() {
+    }
+
+    public static <T> ObservableTransformer<T, T> rxSchedulerHelper() {
+        return (upstream) -> upstream.subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread());
+    }
+}
