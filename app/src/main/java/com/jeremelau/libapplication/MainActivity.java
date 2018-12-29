bcb4ff114c0953f60a9c05be5ef9318612557684
Jereme <jeremegeit@pm.me>
2018-12-29 14:57:26 +0800

    add utils

--- app/src/main/java/com/jeremelau/libapplication/MainActivity.java
+++ app/src/main/java/com/jeremelau/libapplication/MainActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.widget.TextView;
 
 import com.guoguang.flickview.FlickHelper;
+import com.guoguang.utils.IDCardValidateUtil;
+import com.guoguang.utils.PhoneNumUtil;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,5 +17,8 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         TextView textView = findViewById(R.id.hello_world);
         FlickHelper.getFlickHelper().startFlick(textView, 1, 0, 800);
+
+        System.out.println(IDCardValidateUtil.validate_effective("320924199812123456"));
+        System.out.println(PhoneNumUtil.isPhoneNumberValid("+86" + "18888888888", "86"));
     }
 }
---
+++ utils/src/main/java/com/guoguang/utils/PhoneNumUtil.java
@@ -0,0 +1,32 @@
+package com.guoguang.utils;
+
+import com.google.i18n.phonenumbers.NumberParseException;
+import com.google.i18n.phonenumbers.PhoneNumberUtil;
+import com.google.i18n.phonenumbers.Phonenumber;
+
+/**
+ * Author: Created by jereme on 2018/12/29
+ * E-main: liuqx@guoguang.com.cn
+ */
+public class PhoneNumUtil {
+    /**
+     * 根据区号判断是否是正确的电话号码
+     * @param phoneNumber :带国家码的电话号码
+     * @param countryCode :默认国家码
+     * return ：true 合法  false：不合法
+     */
+    public static boolean isPhoneNumberValid(String phoneNumber, String countryCode){
+
+        System.out.println("isPhoneNumberValid: "+phoneNumber+"/"+countryCode);
+        PhoneNumberUtil phoneUtil = PhoneNumberUtil.getInstance();
+        try{
+            Phonenumber.PhoneNumber numberProto = phoneUtil.parse(phoneNumber, countryCode);
+            return phoneUtil.isValidNumber(numberProto);
+        }catch (NumberParseException e){
+            System.err.println("isPhoneNumberValid NumberParseException was thrown: " + e.toString());
+        }
+        return false;
+    }
+
+
+}
